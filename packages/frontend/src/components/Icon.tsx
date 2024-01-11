bf4f80f286cb4e00b2464bdc2003d35568720197
milosz.hozakowski <mhozakowski@gmail.com>
2024-01-11 13:25:54 +0100

    improved empty value handling

--- packages/frontend/src/components/Icon.tsx
+++ packages/frontend/src/components/Icon.tsx
@@ -15,7 +15,7 @@ export default function Icon({ color }: { color: string | undefined }) {
       <path
         d="M7 3.34a10 10 0 1 1 -4.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 4.995 -8.336z"
         stroke-width="0"
-        fill={color ? color : "currentColor"}
+        fill={color || "currentColor"}
       />
     </svg>
   );
