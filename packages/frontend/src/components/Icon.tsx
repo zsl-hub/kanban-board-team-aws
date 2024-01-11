9d8e2c794a089f4d2ae2eba3fec2f9e561b61b78
milosz.hozakowski <mhozakowski@gmail.com>
2024-01-11 14:32:48 +0100

    cleaned up dev console from some minor errors

--- packages/frontend/src/components/Icon.tsx
+++ packages/frontend/src/components/Icon.tsx
@@ -6,15 +6,15 @@ export default function Icon({ color }: { color: string | undefined }) {
       width="22"
       height="22"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path
         d="M7 3.34a10 10 0 1 1 -4.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 4.995 -8.336z"
-        stroke-width="0"
+        strokeWidth="0"
         fill={color || "currentColor"}
       />
     </svg>
