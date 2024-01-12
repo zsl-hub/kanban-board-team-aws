00223da4ec24ba7c741cfd64b97184dc800dc1a4
milosz.hozakowski <mhozakowski@gmail.com>
2024-01-12 14:26:49 +0100

    all colors are in the same format

--- packages/frontend/config/colors.ts
+++ packages/frontend/config/colors.ts
@@ -1,9 +1,9 @@
 const colors = {
-  red: "#da3633",
-  green: "#228435",
-  brown: "#9b6803",
-  orange: "#bd561d",
-  purple: "#8756e1",
+  red: "rgba(218, 54 ,51 ,1)",
+  green: "rgba(34, 132, 53, 1)",
+  brown: "rgba(155, 104, 3, 1)",
+  orange: "rgba(189, 86, 29, 1)",
+  purple: "rgba(135, 86, 225, 1)",
   lightGray: "rgba(235, 236, 240, 1)",
   darkGray: "rgba(45, 55, 72, 0.4)",
 };
