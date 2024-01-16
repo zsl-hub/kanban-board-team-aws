ca7367704bcb1ac55d63d9632089fbe1292d84f5
ArrogantPeter <piotrek.wasiewicz@gmail.com>
2024-01-16 12:18:03 +0100

    added response based on zod error and removed Interface from Task.ts

--- packages/functions/src/lambda/task/add/lambda.ts
+++ packages/functions/src/lambda/task/add/lambda.ts
@@ -1,20 +1,21 @@
 import { v4 as uuidv4 } from "uuid";
 import { ApiResponse } from "src/model/responses";
-import { taskSchema } from "src/model/Task"
+import { Task } from "src/model/Task"
 import { APIGatewayProxyEventV2 } from "aws-lambda";
+import { z } from "zod";
 
 
 export async function main(e: APIGatewayProxyEventV2) {
     const body=JSON.parse(e.body??"")
-
-    const task = {
-        id : uuidv4(),
-        name : body?.name,
-        description : body?.description,
-        columnId : parseInt(body?.columnId),
+    try {
+        body.id = uuidv4();
+        const task = Task.parse(body)
+        return ApiResponse.ok(task);
+    }catch(err){
+        if (err instanceof z.ZodError) {
+            return (err.issues.map(e=>`${e.message} at field ${e.path}`));
+          }
     }
-
-    taskSchema.parse(task);
     
-    return ApiResponse.ok(task);
+    
 }
