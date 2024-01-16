3b3852a3212be6fc03cd07edfd10fdc3d72700d7
ArrogantPeter <piotrek.wasiewicz@gmail.com>
2024-01-16 11:05:47 +0100

    add function stub

--- packages/functions/src/lambda/task/add/lambda.ts
+++ packages/functions/src/lambda/task/add/lambda.ts
@@ -1,16 +1,20 @@
+import { v4 as uuidv4 } from "uuid";
 import { ApiResponse } from "src/model/responses";
+import { taskSchema } from "src/model/Task"
+import { APIGatewayProxyEventV2 } from "aws-lambda";
 
 
-
-export async function main(e: any) {
-    let body = e.queryStringParameters
-    if(typeof(e.body) == typeof("string")) body=JSON.parse(e.body) //handling stringified body
+export async function main(e: APIGatewayProxyEventV2) {
+    const body=JSON.parse(e.body??"")
 
     const task = {
-        name : body.name,
-        description : body.description,
-        columnId : parseInt(body.columnId),
+        id : uuidv4(),
+        name : body?.name,
+        description : body?.description,
+        columnId : parseInt(body?.columnId),
     }
-    return ApiResponse.ok(task);
 
+    taskSchema.parse(task);
+    
+    return ApiResponse.ok(task);
 }
