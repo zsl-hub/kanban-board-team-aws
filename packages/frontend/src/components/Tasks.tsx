fcd51e42cd599467dfd66718e933f7a01968b476
milosz.hozakowski <mhozakowski@gmail.com>
2024-01-16 09:45:15 +0100

    added expand description button

--- packages/frontend/src/components/Tasks.tsx
+++ packages/frontend/src/components/Tasks.tsx
@@ -1,14 +1,5 @@
-import {
-  Flex,
-  Card,
-  CardHeader,
-  CardBody,
-  Text,
-  Button,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import colors from "../../config/colors";
-import { useState } from "react";
+import { Flex } from "@chakra-ui/react";
+import TaskComponent from "./Task";
 
 interface Task {
   id: number;
@@ -22,51 +13,10 @@ interface TaskProps {
 }
 
 export default function Tasks({ tasks }: TaskProps) {
-  const value = useColorModeValue(colors.lightGray, colors.veryDarkGray);
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  function handleExpand() {
-    setIsExpanded(!isExpanded);
-  }
-
   return (
     <Flex flexDirection="column" gap="0.75rem">
       {tasks.map((task) => (
-        <Card className="task" key={task.id}>
-          <CardHeader
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-            fontWeight="500"
-            borderBottom={`1px solid ${value}`}
-          >
-            <Text>{task.name}</Text>
-
-            <Button variant="outline" size="xs">
-              X
-            </Button>
-          </CardHeader>
-
-          <CardBody
-            display="flex"
-            flexDirection="column"
-            alignItems="flex-start"
-            gap="0.5rem"
-          >
-            <Text>{`${task.description
-              .split(" ")
-              .slice(0, 6)
-              .join(" ")}`}</Text>
-            <Button
-              size="s"
-              fontSize="s"
-              padding="0.2em 0.5em"
-              onClick={handleExpand}
-            >
-              {isExpanded ? "show less" : "show more"}
-            </Button>
-          </CardBody>
-        </Card>
+        <TaskComponent key={task.id} task={task} />
       ))}
     </Flex>
   );
