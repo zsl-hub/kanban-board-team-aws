85d52bbc9998098499d188151a7c555d7150038e
ArrogantPeter <piotrek.wasiewicz@gmail.com>
2024-01-24 10:33:44 +0100

    update update and update update unit test

--- packages/functions/src/lambda/task/update/lambda.test.ts
+++ packages/functions/src/lambda/task/update/lambda.test.ts
@@ -7,24 +7,70 @@ import TaskRepository from '@kanban-board-team-aws/functions/repositories/taskRe
 const taskMock = {id: "7e76c64a-7270-4a65-8f17-fe9ac3187b1a",name: "name",description : "testDesc",columnId: 1, order: 1} as Task;
 
 describe("/task/update tests", ()=>{
-    test(`Should return status code 200 and "Updated task" message`, async ()=>{
+
+    afterEach(_=>{
+        vi.restoreAllMocks()
+    })
+
+    test(`should return status code 200 and "Updated task" message`, async ()=>{
+
+
+        
+        // GIVEN
         vi.spyOn(TaskRepository.prototype, "put").mockResolvedValue()
 
         const event: APIGatewayProxyEventV2 = {
-            queryStringParameters: {
+            body: JSON.stringify(taskMock)
+        } as any
+
+        // WHEN
+        const result = await main(event)
+
+        // THEN
+        expect(result?.statusCode ?? 0).toBe(200)
+        expect(JSON.parse(result?.body ?? "")).toBe(`Updated task.`)
+    })
+
+    test.each([
+        ["missing body fields", 
+            {a: "1"}
+        ],
+        ["incorrect body fields",
+            {
                 id: taskMock.id,
                 name: taskMock.name,
                 description: taskMock.description,
-                columnId: taskMock.columnId,
-                order: taskMock.order,
+                columnId: "this shouldn't be a string",
+                order: taskMock.order
             }
+        ]
+    ])(`should return status code 400 - %s`, async (description, body)=>{       
+        // GIVEN
+        vi.spyOn(TaskRepository.prototype, "put").mockResolvedValue()
+
+        const event: APIGatewayProxyEventV2 = {
+            body: JSON.stringify(body)
         } as any
 
+        //WHEN
         const result = await main(event)
 
-        expect(result?.statusCode ?? 0).toBe(200)
-        expect(JSON.parse(result?.body ?? "")).toBe(`Updated task.`)
+        // THEN
+        expect(result?.statusCode ?? 0).toBe(400)
+    })
 
+    test("should return status code 500", async () => {
+        // GIVEN
+        vi.spyOn(TaskRepository.prototype, "put").mockRejectedValue(undefined)
+
+        const event: APIGatewayProxyEventV2 = {
+            body: JSON.stringify(taskMock)
+        } as any
+
+        //WHEN
+        const result = await main(event)
+
+        // THEN
         expect(result?.statusCode ?? 0).toBe(500)
     })
 })
--- packages/functions/src/lambda/task/update/lambda.ts
+++ packages/functions/src/lambda/task/update/lambda.ts
@@ -1,6 +1,7 @@
 import { TaskSchema } from "@kanban-board-team-aws/functions/model/Task";
 import { ApiResponse } from "@kanban-board-team-aws/functions/model/responses";
 import TaskRepository from "@kanban-board-team-aws/functions/repositories/taskRepository";
+import { z } from "zod";
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 import { ApiError } from "src/model/errors";
 
@@ -8,17 +9,23 @@ import { ApiError } from "src/model/errors";
 const taskRepository = TaskRepository.getTaskRepository();
 
 export async function main(e: APIGatewayProxyEventV2) {
-    
+        
     try {
         const body=JSON.parse(e.body??"")
+        const task = TaskSchema.parse(body)
         const itemCount = taskRepository.getById(body.id)
         if (!itemCount) return ApiResponse.notFound(`Task with id ${body.id} was not found!`);
 
-        const task = TaskSchema.parse(body)
+        
         await taskRepository.put(task);
         const res = "Updated task."
         return ApiResponse.ok(res)
     }catch(err){
+        if (err instanceof z.ZodError) {
+            const res = err.issues.map(e=>`${e.message} at field ${e.path}`)
+            const apiError = new ApiError(400, res.join(";")) 
+            return apiError.getApiResponse()
+        }   
         if (err instanceof ApiError) {
             return err.getApiResponse()
         }
